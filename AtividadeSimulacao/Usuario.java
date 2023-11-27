5528000e0bd3a61b712a2e25fb9669771d5b141d
kayquepiton <kayquepiton@gmail.com>
2023-11-27 11:11:35 -0300

    Atividade Simulação

--- AtividadeSimulacao/Usuario.java
+++ AtividadeSimulacao/Usuario.java
@@ -5,7 +5,7 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
-    private List<Postagem> listaPostagens;
+    private List<String> listaPostagens; // Alterado para List<String>
     private List<Sessao> listaSessoes;
     private List<Usuario> amigos;
 
@@ -13,7 +13,7 @@ public class Usuario {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.listaPostagens = new ArrayList<>();
+        this.listaPostagens = new ArrayList<>(); // Inicializado como ArrayList de String
         this.listaSessoes = new ArrayList<>();
         this.amigos = new ArrayList<>();
     }
