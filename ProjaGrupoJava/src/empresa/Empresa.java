6d96a6144b97054961c900db1df951ca2ea9e25e
Caua-Clemente <cauaclementelima@gmail.com>
2023-12-06 16:39:53 -0300

    Subindo estrutura base do codigo java

---
+++ ProjaGrupoJava/src/empresa/Empresa.java
@@ -0,0 +1,132 @@
+package empresa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Empresa {
+	private List<Cliente> listaClientes = new ArrayList<Cliente>();
+    private List<Imovel> listaImoveis = new ArrayList<Imovel>();
+    private List<Fatura> listaFaturas = new ArrayList<Fatura>();
+    private List<Falha> listaFalhas = new ArrayList<Falha>();
+
+    public void menuEmpresa() {
+    	
+    }
+    
+    public void menuCliente() {
+    	
+    }
+    
+    public void incluirCliente() {
+    	
+    }
+    
+    public void consultarCliente() {
+    	
+    }
+    
+    public void listarCliente() {
+    	
+    }
+    
+    public void excluirCliente() {
+    	
+    }
+    
+    public void alterarCliente() {
+    	
+    }
+    
+	
+    public void menuImovel() {
+    	
+    }
+    
+    public void incluirImovel() {
+    	
+    }
+    
+    public void consultarImovel() {
+    	
+    }
+    
+    public void listarImovel() {
+    	
+    }
+    
+    public void excluirImovel() {
+    	
+    }
+    
+    public void alterarImovel() {
+    	
+    }
+    
+	
+    public void menuFaturas() {
+    	
+    }
+    
+    public void incluirFatura() {
+    	
+    }
+
+    public void listarTodasFaturas() {
+    	
+    }
+    
+    public void listarFaturasAbertas() {
+    	
+    }
+    
+	
+    public void menuPagamento() {
+    	
+    }
+    
+    public void incluirPagamento() {
+    	
+    }
+    
+    public void listarTodosPagamentos() {
+    	
+    }
+    
+    public void listarPagamentosFatura() {
+    	
+    }
+    
+    public void ListarReembolsos() {
+    	
+    }
+    
+    public void consultarReembolsoFatura() {
+    	
+    }
+    
+	
+    public void menuFalhas() {
+    	
+    }
+    
+    public void incluirFalhaComMatricula() {
+    	
+    }
+    
+    public void incluirFalhaSemMatricula() {
+    	
+    }
+    
+    public void menuReparos() {
+    	
+    }
+    
+    public void listarReparosAbertos() {
+    	
+    }
+    
+    public void encerrarReparo() {
+    	
+    }
+    
+}
